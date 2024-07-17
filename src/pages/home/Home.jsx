bebe0dec1ea12db7bdf44de31a646fea96c05545
makwanaom <117265350+makwanaom@users.noreply.github.com>
2024-07-17 19:18:20 +0530

    Api fetched

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import Sidebar from '../../components/sidebar/Sidebar';
+import MessageContainer from '../../components/messages/MessageContainer';
+
+
+const Home = () => {
+
+  return (
+    <div>
+
+      <div className='  flex h-96 sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+
+
+        <Sidebar />
+
+        <MessageContainer />
+
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
